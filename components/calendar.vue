0d879a22a3716936ff199d780b9d091e1718ee3a
fred <ugodaycnzass1995@gmail.com>
2023-11-01 16:38:11 +0300

    fix

--- components/calendar.vue
+++ components/calendar.vue
@@ -13,9 +13,7 @@ const attrs = ref([
       fillMode: "solid",
     },
     dates,
-  
   },
-  
 ]);
 </script>
 
@@ -29,40 +27,44 @@ const attrs = ref([
 
 
 <style>
-
-
-.vc-title span{
+.vc-title span {
   --tw-text-opacity: 1;
   color: rgba(113, 48, 163, var(--tw-text-opacity));
   font-size: 20px;
   font-weight: bold;
 }
+
 .vc-header {
   margin-bottom: 10px;
 }
-.vc-highlight, .vc-day-content {
+
+.vc-highlight,
+.vc-day-content {
   width: 40px;
   height: 40px;
   border-radius: 10px;
 }
+
 .vc-highlight-content-solid {
-  
+
   color: white !important;
 }
+
 .vc-day {
   min-height: 52px;
 
 }
+
 .vc-container {
-border: none;
-background: rgb(255,255,255);
-background: linear-gradient(0deg, rgba(255,255,255,1) 0%, rgba(215,233,255,1) 100%); 
+  border: none;
+  background: rgb(255, 255, 255);
+  background: linear-gradient(0deg, rgba(255, 255, 255, 1) 0%, rgba(215, 233, 255, 1) 100%);
 
 
 }
+
 .vc-day-content {
   font-size: 20px;
   font-weight: bold;
   color: #3eb1b8;
-}
-</style>
+}</style>
